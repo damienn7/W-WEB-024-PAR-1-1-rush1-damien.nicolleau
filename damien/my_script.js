a0cabbf332eeecbfa437d4d392dad6e29e07f7d8
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-27 23:09:21 +0100

    local changes to commit before checkout

--- damien/my_script.js
+++ damien/my_script.js
@@ -1,11 +1,45 @@
-function ValidateEmail(mail)
-{
+document
+.getElementById("inscription")
+.addEventListener("submit", function validateform(e) {
+  let erreur;
 
-if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-{
-return (true)
-}
-alert("You have entered an invalid email address!")
-return false
+  let sexe = document.getElementById("sexe");
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let telephone = document.getElementById("telephone");
+  let website = document.getElementById("website");
+  let age = document.getElementById("age");
+  let hobbies = document.getElementById("hobbies");
 
-}
+  if (!hobbies.value) {
+    erreur = "Veuillez renseigner au moins un hobbie";
+  }
+  if (!age.value) {
+    erreur = "Veuillez renseigner votre age";
+  }
+
+  if (!website.value) {
+    erreur = "Veuillez renseigner votre website";
+  }
+  if (!telephone.value) {
+    erreur = "Veuillez renseigner votre telephone";
+  }
+  if (!sexe.value) {
+    erreur = "Veuillez renseigner votre sexe";
+  }
+  if (!email.value) {
+    erreur = "Veuillez renseigner votre email";
+  }
+  if (!name.value) {
+    erreur = "Veuillez renseigner votre nom";
+  }
+
+  if (erreur) {
+    e.preventDefault(); //empeche le comporte defaut du formulaire
+    document.getElementById("erreur").innerHTML = erreur; //affiche le message d'erreur
+
+    return false; // rien ne ce passe et le formulaire ne s'envoie pas
+  } else {
+    alert("Formulaire envoyé !"); //si pas d'erreur formulaire envoyé
+  }
+});
